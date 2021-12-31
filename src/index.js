1d5fd3e2436ecc101b2ed760d0f33f248db26b2d
Glossawy <4606234+Glossawy@users.noreply.github.com>
2021-12-30 20:12:46 -0500

    dimmers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import m from 'mithril';
-import { token, wifi, address, media_sources, groupname, refreshinterval } from './config.json';
+import Promise from 'promise-polyfill';
+import { token, wifi, address, media_sources, dimmer_entities, groupname, refreshinterval } from './config.json';
 import './style.css';
 
 const errdiv = document.createElement('div');
@@ -8,9 +9,33 @@ const container = document.createElement('div');
 document.body.appendChild(errdiv);
 document.body.appendChild(container);
 
+function after(waitms) {
+  return new Promise(r => setTimeout(() => r(waitms), waitms));
+}
+
+function debounce(inner, ms = 0) {
+  let timer = null;
+  let resolves = [];
+
+  return function (...args) {
+    // Run the function after a certain amount of time
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      // Get the result of the inner function, then apply it to the resolve function of
+      // each promise that has been created since the last time the inner function was run
+      let result = inner(...args);
+      resolves.forEach(r => r(result));
+      resolves = [];
+    }, ms);
+
+    return new Promise(r => resolves.push(r));
+  };
+}
+
 var Entities = {
   switches: [],
   lights: [],
+  dimmers: [],
   scenes: [],
   sensors: [],
   media_players: [],
@@ -33,10 +58,16 @@ var Entities = {
         });
       Entities.lights = [];
       entities
-        .filter((entity_id) => entity_id.startsWith('light'))
+        .filter((entity_id) => entity_id.startsWith('light') && !dimmer_entities.includes(entity_id)  )
         .forEach((entity_id) => {
           Entities.lights.push(result.filter((item) => item.entity_id === entity_id)[0]);
         });
+      Entities.dimmers = [];
+      entities
+      .filter((entity_id) => entity_id.startsWith('light') )
+      .forEach((entity_id) => {
+        Entities.dimmers.push(result.filter((item) => item.entity_id === entity_id)[0]);
+      });
       Entities.scenes = [];
       entities
         .filter((entity_id) => entity_id.startsWith('scene'))
@@ -189,9 +220,92 @@ class Light {
   }
 }
 
+var postHomeAssistantServiceImmediate = function(service, action, data) {
+  return m.request({
+    method: 'POST',
+    url: `${address}/api/services/${service}/${action}`,
+    headers: { authorization: `Bearer ${token}` },
+    data
+  });
+}
+
+var postHomeAssistantService = debounce(postHomeAssistantServiceImmediate, 100);
+
+class Dimmer {
+  view({ attrs: { attributes, entity_id, state } }) {
+    const name = attributes.friendly_name || entity_id
+    const { brightness, color_temp } = Object.assign({ brightness: 0, color_temp: 0 }, attributes)
+    const dimmer = Entities.dimmers.find((item) => item.entity_id === entity_id);
+
+    const dimmerToggle =
+      m(`.toggle.${state}`,
+        {
+          onclick: () => {
+            const targetState = (state === 'on' ? 'off' : 'on')
+            postHomeAssistantService('light', `turn_${targetState}`, { entity_id: entity_id }).then(() => {
+              dimmer.state = targetState;
+              dimmer.attributes.brightness = (targetState === 'on' ? 255 : 0);
+              m.redraw();
+            });
+          }
+        },
+        [
+          m('div', 'On'),
+          m('div', 'Off')
+        ]
+      );
+
+    const dimmerSwitch =
+      m(
+        '.toggle',
+        {
+          style: {
+            'background-color': state == 'on' ? 'black' : 'white',
+            color: state == 'on' ? 'white' : 'black',
+          },
+          onclick: () => {
+            const targetState = (state === 'on' ? 'off' : 'on')
+            postHomeAssistantService('light', `turn_${targetState}`, { entity_id: entity_id }).then(() => {
+              dimmer.state = targetState;
+              dimmer.attributes.brightness = (targetState === 'on' ? 255 : 0);
+              m.redraw();
+            });
+          },
+        },
+        name
+      );
+
+    const dimmerSlider = m(
+          "input[type='range'].slider",
+          {
+            name: entity_id,
+            min: '1', max: '255', value: brightness, disabled: (state === 'off'),
+            oninput: (evt) => {
+              var newBrightness = evt.target.value;
+              var dimmerPostData = { entity_id: entity_id, brightness: newBrightness, transition: 1 }
+
+              postHomeAssistantService('light', 'turn_on', dimmerPostData).then(() => {
+                var dimmer = Entities.dimmers.find((item) => item.entity_id === entity_id);
+                dimmer.attributes.brightness = newBrightness;
+                m.redraw();
+              });
+            }
+          }
+        );
+    return m(
+      '.dimmer',
+      [
+        dimmerSwitch,
+        dimmerSlider
+      ]
+    )
+  }
+}
+
 class Scene {
   view({ attrs: { attributes, entity_id, state = 'off' } }) {
     var name = attributes.friendly_name || entity_id;
+
     return m(
       '.switch',
       {
@@ -259,6 +373,9 @@ class Layout {
         ...Entities.switches.map((switchData) => m(Switch, switchData)),
         ...Entities.lights.map((lightData) => m(Light, lightData)),
         ...Entities.scenes.map((sceneData) => m(Scene, sceneData)),
+      ]),
+      m('.switch-row', [
+        ...Entities.dimmers.map((dimmerData) => m(Dimmer, dimmerData))
       ]),
       ...Entities.media_players.map((mediaPlayerData) => m(MediaPlayer, mediaPlayerData)),
       wifi ? m(Overlay, { label: 'wifi' }, m('img.wifi', { src: wifi })) : '',
